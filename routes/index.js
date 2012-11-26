3cd3b3192889e79eae2d8ff04cfb4e34a18708a8
Russ Bradberry <rbradberry@gmail.com>
2012-11-26 16:14:11 -0500

    don't throw mid-app

--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,36 @@
  * GET users listing.
  */
 
-exports.index = function(req, res){
+exports.index = function(req, res, next){
   req.app.get('cassandra').cql('SELECT * FROM users LIMIT 10', function(err, users){
     if(err){
-      throw(err);
+      return next(err);
     }
 
     res.render('index', { title: 'Users', users: users });
   });
 };
 
-exports.new = function(req, res){
+exports.new = function(req, res, next){
   var insert = 'UPDATE users SET first_name=?, last_name=? WHERE email=?',
       params = [req.body.first_name, req.body.last_name, req.body.email];
 
   req.app.get('cassandra').cql(insert,  params, function(err, users){
     if(err){
-      throw(err);
+      return next(err);
     }
 
     res.redirect('/');
   });
 };
 
-exports.delete = function(req, res){
+exports.delete = function(req, res, next){
   var remove = 'DELETE FROM users WHERE email=?',
       params = [req.body.email];
 
   req.app.get('cassandra').cql(remove,  params, function(err, users){
     if(err){
-      throw(err);
+      return next(err);
     }
 
     res.redirect('/');
